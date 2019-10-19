de39ac8b19beba6368f6d7eed64c4e09c58dd0d0
ahirner <a.hirner@gmail.com>
2019-10-20 01:07:47 +0200

    change rank convention

--- MPC_identities.py
+++ MPC_identities.py
@@ -1,5 +1,5 @@
 # Define MPC identities for model-hiding:
 # PREDICTOR has access to image classification model parameters, PATIENT has access to data samples to be classified
 # Hence, our world_size is 2
-PATIENT=0
-PREDICTOR=1
+PATIENT=1
+PREDICTOR=0
